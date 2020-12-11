7b75b08d164186831ac27a5b6f8640fc3048adca
Nataliya Zhuravleva <natalindria@gmail.com>
2020-12-11 14:53:51 -0800

    Change _PastryDealMarker to public with get. Tests are confirmed.

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -3,28 +3,28 @@ namespace Bakery.Models
   public class Pastry
   {
     public double PastryPrice { get; set; }
-    private bool _PastryDealMarker;
+    private bool PastryDealMarker { get; }
 
     public Pastry(double pastryPrice)
     {
       PastryPrice = pastryPrice;
     }
 
-    public Pastry(double pastryPrice, bool _pastryDealMarker)
+    public Pastry(double pastryPrice, bool pastryDealMarker)
       : this(pastryPrice)
     {
-      _PastryDealMarker = _pastryDealMarker; //false - no deals, true - we have a deal
+      PastryDealMarker = pastryDealMarker; //false - no deals, true - we have a deal
     }
 
     public double CalculatePastryCost(int amount)
     {
-      if (_PastryDealMarker == false || amount < 3)
+      if (PastryDealMarker == false || amount < 3)
       {
         return amount * PastryPrice;
       }
       else
       {
-        return (amount/3)*(PastryPrice-1)+(amount-amount/3)*PastryPrice;
+        return (amount / 3) * (PastryPrice - 1) + (amount - amount / 3) * PastryPrice;
       }
     }
   }
